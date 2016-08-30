c3827b48661973d136214c79c9fe221beb3d04c3
chenmeng <1752397568@qq.com>
2016-08-30 18:27:54 +0800

    change sae config,try to link sinacloud mysql

--- ThinkPHP/Extend/Engine/Sae/SaeImit/config.php
+++ ThinkPHP/Extend/Engine/Sae/SaeImit/config.php
@@ -7,10 +7,10 @@
 // $Id: config.php 903 2012-03-16 03:50:22Z luofei614@126.com $
 $appConfig=  include APP_PATH.'Conf/config.php';
 return array(
-    'db_host'=>isset($appConfig['DB_HOST'])?$appConfig['DB_HOST']:'localhost',
-    'db_user'=>isset($appConfig['DB_USER'])?$appConfig['DB_USER']:'root',
-    'db_pass'=>isset($appConfig['DB_PWD'])?$appConfig['DB_PWD']:'',
-    'db_name'=>isset($appConfig['DB_NAME'])?$appConfig['DB_NAME']:'sae',
+    'db_host'=>isset($appConfig['DB_HOST'])?$appConfig['DB_HOST']:'w.rdc.sae.sina.com.cn',
+    'db_user'=>isset($appConfig['DB_USER'])?$appConfig['DB_USER']:'noy3mylyoz',
+    'db_pass'=>isset($appConfig['DB_PWD'])?$appConfig['DB_PWD']:'xyk3khxw0430mi302mkmzwh3mj1j0ylwjx153ilk',
+    'db_name'=>isset($appConfig['DB_NAME'])?$appConfig['DB_NAME']:'app_oncm',
     'db_charset'=>isset($appConfig['DB_CHARSET'])?$appConfig['DB_CHARSET']:'utf8',
     'storage_url'=>trim(dirname($_SERVER['SCRIPT_NAME']),'/\\').'/',
     'storage_dir'=>'./',
