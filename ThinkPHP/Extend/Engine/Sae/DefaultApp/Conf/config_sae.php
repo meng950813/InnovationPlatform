efd470d9f59f26983ef10cf8beb9a94453c9dae2
chenmeng <1752397568@qq.com>
2016-09-06 23:20:14 +0800

    先行版V1.4

--- ThinkPHP/Extend/Engine/Sae/DefaultApp/Conf/config_sae.php
+++ ThinkPHP/Extend/Engine/Sae/DefaultApp/Conf/config_sae.php
@@ -2,6 +2,7 @@
 return array(
    'TMPL_PARSE_STRING'=>array(
       // __PUBLIC__/upload  -->  /Public/upload -->http://appname-public.stor.sinaapp.com/upload
-   '/Public/upload'=>sae_storage_root('Public').'/upload'
+   // '/Public/upload'=>sae_storage_root('Public').'/upload'
+   '/Uploads'=>sae_storage_root('Uploads')
     )
 );
