85cbee640766fabdf59f90af6b13e6bce8e7ee34
meng950813 <1752397568@qq.com>
2016-09-29 12:22:02 +0800

    优化 personal 模块页面

--- Public/js/personal_calss_main.js
+++ Public/js/personal_calss_main.js
@@ -1,8 +1,8 @@
 (function(){
     var bodyHeight = document.body.clientHeight;
     var bottombox = document.getElementById('bottombox');
-    var contentHeight = document.getElementById('body').clientHeight;
-    if((bodyHeight-contentHeight-130)>60){
+    var contentHeight = document.getElementsByClassName('main')[0].clientHeight;
+    if((bodyHeight-contentHeight-210)>60){
         bottombox.className="buttom_fixed";
     }
 })();
