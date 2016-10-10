f151849f1ebe3111d626348b52c33f8ae2362d0f
meng950813 <1752397568@qq.com>
2016-10-10 13:06:44 +0800

    change min-height from 600/500px to 300px

--- Public/js/content_main.js
+++ Public/js/content_main.js
@@ -1,8 +1,10 @@
 var bodyHeight = document.body.clientHeight;
 var bottombox = document.getElementById('bottombox');
 var contentHeight = document.getElementById('main').clientHeight;
+console.log(contentHeight,bodyHeight);
 if((bodyHeight-contentHeight-130)>80){
     var next = document.getElementsByClassName('next')[0];
+    console.log("fasdfasd");
     next.style.position="fixed";
     next.style.bottom = "80px";
     bottombox.className="buttom_fixed";
